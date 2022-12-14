f04180978dd0297846e27712b65747bc7d048b9c
Robert812218 <robert812@shaw.ca>
2022-12-13 17:08:05 -0700

    update

--- src/components/BlogComponents/Header.js
+++ src/components/BlogComponents/Header.js
@@ -4,7 +4,7 @@ import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import SearchIcon from '@mui/icons-material/Search';
-import Typography from '@mui/icons-material/Typography';
+import Typography from '@mui/material/Typography';
 import Link from '@mui/material/Link';
 
 function Header(props) {
---
+++ src/components/BlogComponents/Main.js
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import Markdown from './Markdown';
+ 
+function Main() {
+    const { posts, title } = props;
+
+    return (
+        <Grid
+            item
+            xs={12}
+            md={8}
+            sx={{
+        />
+    );
+}
